a086f27addd64d82b25aeb6444e72af75ab162e6
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-11-06 15:20:42 -0200

    Add result consolidation for several methods

--- AntiSpamSmile/src/main/java/io/github/marcelovca90/runner/Main.java
+++ AntiSpamSmile/src/main/java/io/github/marcelovca90/runner/Main.java
@@ -10,6 +10,7 @@ import io.github.marcelovca90.helper.MethodHelper;
 import io.github.marcelovca90.helper.ValidationHelper;
 import smile.classification.Classifier;
 import smile.classification.DecisionTree;
+import smile.classification.RandomForest;
 import smile.data.AttributeDataset;
 
 public class Main
@@ -18,52 +19,61 @@ public class Main
     { "rawtypes", "unchecked" })
     public static void main(String[] args) throws Exception
     {
-        for (int features = 8; features <= 1024; features *= 2)
+        Class<? extends Classifier>[] clazzes = new Class[]
         {
-            // TODO: load file names from metadata
-            final String filenameHam = "C:\\Users\\marcelovca90\\git\\anti-spam-weka-data\\2017_BASE2\\2017_BASE2_TREC\\MI\\" + features + "\\ham";
-            final String filenameSpam = "C:\\Users\\marcelovca90\\git\\anti-spam-weka-data\\2017_BASE2\\2017_BASE2_TREC\\MI\\" + features + "\\spam";
+                DecisionTree.class, RandomForest.class
+        };
 
-            // read data
-            AttributeDataset ham = DatasetHelper.read(filenameHam, ClassType.HAM);
-            AttributeDataset spam = DatasetHelper.read(filenameSpam, ClassType.SPAM);
-            AttributeDataset dataset = DatasetHelper.mergeDataSets(ham, spam);
-
-            // select features
-            dataset = FeatureSelectionHelper.sumSquaresRatio(dataset);
-
-            // initialize rng seed
-            int seed = 2;
-
-            System.out.println(DecisionTree.class.getName() + " with " + features + " features");
-
-            // run 10 executions
-            for (int run = 0; run < 10; run++)
+        for (Class clazz : clazzes)
+        {
+            for (int features = 8; features <= 1024; features *= 2)
             {
-                // shuffle data
-                dataset = DatasetHelper.shuffle(dataset, seed);
+                // TODO: load file names from metadata
+                final String filenameHam = "C:\\Users\\marcelovca90\\git\\anti-spam-weka-data\\2017_BASE2\\2017_BASE2_SPAM_ASSASSIN\\MI\\" + features + "\\ham";
+                final String filenameSpam = "C:\\Users\\marcelovca90\\git\\anti-spam-weka-data\\2017_BASE2\\2017_BASE2_SPAM_ASSASSIN\\MI\\" + features + "\\spam";
 
-                // build train/test data
-                Pair<AttributeDataset, AttributeDataset> pair = DatasetHelper.split(dataset, 0.5);
-                AttributeDataset train = pair.getLeft();
-                AttributeDataset test = pair.getRight();
-                double[][] trainx = train.toArray(new double[train.size()][]);
-                int[] trainy = train.toArray(new int[train.size()]);
-                double[][] testx = test.toArray(new double[test.size()][]);
-                int[] testy = test.toArray(new int[test.size()]);
+                // read data
+                AttributeDataset ham = DatasetHelper.read(filenameHam, ClassType.HAM);
+                AttributeDataset spam = DatasetHelper.read(filenameSpam, ClassType.SPAM);
+                AttributeDataset dataset = DatasetHelper.mergeDataSets(ham, spam);
 
-                // train and test classifier
-                MethodHelper.init(trainx, trainy);
-                Classifier classifier = MethodHelper.forClass(DecisionTree.class);
-                ValidationHelper.aggregate(classifier, testx, testy);
+                // select features
+                dataset = FeatureSelectionHelper.sumSquaresRatio(dataset);
 
-                // update rng seed
-                seed = Primes.nextPrime(seed + 1);
+                // initialize rng seed
+                int seed = 2;
+
+                System.out.println(clazz.getName() + " with " + features + " features");
+                Thread.sleep(3000);
+
+                // run 10 executions
+                for (int run = 0; run < 10; run++)
+                {
+                    // shuffle data
+                    dataset = DatasetHelper.shuffle(dataset, seed);
+
+                    // build train/test data
+                    Pair<AttributeDataset, AttributeDataset> pair = DatasetHelper.split(dataset, 0.5);
+                    AttributeDataset train = pair.getLeft();
+                    AttributeDataset test = pair.getRight();
+                    double[][] trainx = train.toArray(new double[train.size()][]);
+                    int[] trainy = train.toArray(new int[train.size()]);
+                    double[][] testx = test.toArray(new double[test.size()][]);
+                    int[] testy = test.toArray(new int[test.size()]);
+
+                    // train and test classifier
+                    MethodHelper.init(trainx, trainy);
+                    Classifier classifier = MethodHelper.forClass(clazz);
+                    ValidationHelper.aggregate(classifier, testx, testy);
+
+                    // update rng seed
+                    seed = Primes.nextPrime(seed + 1);
+                }
+
+                ValidationHelper.consolidate(clazz);
+
+                System.out.println();
             }
-
-            ValidationHelper.consolidate(DecisionTree.class);
-
-            System.out.println();
         }
     }
 }
