082d4ea74c9f914f330dbd3c7265c8a1f83118c7
Marcelo Cysneiros <marcelovca90@gmail.com>
2017-11-06 19:24:52 -0200

    Add maven-assembly-plugin

---
+++ AntiSpamSmile/runMacOS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TOTAL_RAM_B=$(sysctl hw.memsize | cut -d ' ' -f 2)
+TOTAL_RAM_KB=$((${TOTAL_RAM_B}/1024))
+TOTAL_RAM_MB=$((${TOTAL_RAM_KB}/1024))
+TOTAL_RAM_GB=$((${TOTAL_RAM_MB}/1024))
+
+HEAP_SIZE="${TOTAL_RAM_GB}G"
+STACK_SIZE="${TOTAL_RAM_GB}m"
+GC_TYPE="+UseG1GC"
+JAR_PATH="./target/AntiSpamSmile-0.0.1-SNAPSHOT.jar"
+
+java -Xmx${HEAP_SIZE} -Xss${STACK_SIZE} -XX:${GC_TYPE} -jar ${JAR_PATH}
